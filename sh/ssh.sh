91dcc2eaf1df5f1b930b25a8a3c0623de9b78d48
Berk D. Demir <bdd@mindcast.org>
2019-12-27 00:50:17 -0800

    sh/ssh: RC-file helper for using ssh via gpg-agent
    If we're using GPG Agent for SSH keys, call `ssh:gpg.rc` from
    ~/.(ba|z)shrc.local to get SSH_AUTH_SOCK point to gpg-agent's socket.
    
    It's not easy (/possible?) to get launchd set a different SSH_AUTH_SOCK
    without disabling SIP, hence this.

--- sh/ssh.sh
+++ sh/ssh.sh
@@ -2,6 +2,15 @@
 # macOS only
 [[ $OSTYPE =~ ^darwin ]] || return
 
+ssh:gpg.rc() {
+  # Consider calling this from ~/.(ba|z)shrc.local
+  if hash gpg-connect-agent 2>/dev/null; then
+    gpg-connect-agent /bye
+    typeset -gr LAUNCHD_SSH_AUTH_SOCK=${SSH_AUTH_SOCK}
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  fi
+}
+
 typeset -gr MACOS_PKCS11_PROVIDER=/usr/lib/ssh-keychain.dylib
 ssh:sc-add() {
   ssh-add -s "${MACOS_PKCS11_PROVIDER}"
