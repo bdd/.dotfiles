5fddee5fee3006d089b7729b76dc05bb837ee597
Berk D. Demir <bdd@mindcast.org>
2020-01-18 16:28:36 -0800

    sh/any: Use shell parameter expansions instead of sed

--- sh/any.sh
+++ sh/any.sh
@@ -1,19 +1,19 @@
 # shellcheck shell=bash
 any() {
   if [[ -n "$1" ]]; then
-    # convert 'thing' to '[t]hing' for 'ps | grep [t]hing' trick
-    search=$(sed 's/./[&]/' <<< "$1")
-    replace='&'
+    # convert 'needle' to '[n]eedle' for 'ps | grep [n]eedle' trick
+    local needle=$1
+    local head=${needle:0:1} tail=${needle:1}
 
     # colorize match if stdout is a terminal, and supports 8 or more colors
     if [[ -t 1 && $(tput colors) -ge 8 ]]; then
-      color=$'\e[0;31m'  # red
-      reset=$'\e[0m'
-      replace="${color}${replace}${reset}"
+      local color=$'\e[0;31m'  # red
+      local reset=$'\e[0m'
+      local replace="${color}&${reset}"
     fi
 
     # print header (first line) of 'ps' and matches
-    ps auxwww | sed -n "1p; s/${search}/${replace}/gp"
+    ps auxwww | sed -n "1p; s/[${head}]${tail}/${replace}/gp"
   else
     return 64 # EX_USAGE
   fi
