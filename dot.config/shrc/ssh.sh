667603b4976df0fa5ea361fdc500ef67742fadf3
Berk D. Demir <bdd@mindcast.org>
2020-04-12 00:38:12 -0700

    shrc/ssh: Add support for Fedora and Debian

--- dot.config/shrc/ssh.sh
+++ dot.config/shrc/ssh.sh
@@ -1,21 +1,47 @@
 # shellcheck shell=bash
-# macOS only
-[[ $OSTYPE =~ ^darwin ]] || return 0
 
 ssh:gpg.rc() {
   # Consider calling this from ~/.(ba|z)shrc.local
   if hash gpg-connect-agent 2>/dev/null; then
     gpg-connect-agent /bye
-    typeset -gr LAUNCHD_SSH_AUTH_SOCK=${SSH_AUTH_SOCK}
+    if [[ ${OSTYPE} =~ ^darwin ]]; then
+      typeset -gr LAUNCHD_SSH_AUTH_SOCK=${SSH_AUTH_SOCK}
+    fi
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
   fi
 }
 
-typeset -gr MACOS_PKCS11_PROVIDER=/usr/lib/ssh-keychain.dylib
+
+case "${OSTYPE}" in
+  darwin*)
+    typeset -gr PKCS11_PROVIDER=/usr/lib/ssh-keychain.dylib
+    ;;
+
+  linux*)
+    _dirs=(
+      /usr/lib64/pkcs11  # fedora
+      /usr/lib/x86_64-linux-gnu  # debian
+    )
+
+    for _dir in ${_dirs[@]}; do
+      _so="${_dir}/opensc-pkcs11.so"
+      if [[ -r "${_so}" ]]; then
+        typeset -gr PKCS11_PROVIDER="${_so}"
+        unset _so
+      fi
+    done
+
+    unset _dir _dirs
+    ;;
+
+  *)
+    return 0
+esac
+
 ssh:sc-add() {
-  ssh-add -s "${MACOS_PKCS11_PROVIDER}"
+  ssh-add -s "${PKCS11_PROVIDER}"
 }
 
 ssh:sc-kill() {
-  ssh-add -e "${MACOS_PKCS11_PROVIDER}"
+  ssh-add -e "${PKCS11_PROVIDER}"
 }
