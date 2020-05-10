720e0d0663d25da17f68e73cad3b2f785ddb6a80
Berk D. Demir <bdd@mindcast.org>
2020-05-10 13:55:25 -0700

    Speed up zsh login: homebrew.sh
    Running `brew --prefix` when we find `brew` in one of possible Homebrew
    prefixes is costly at login. This Ruby script may take quite a bit of
    time to run with cold filesystem cache.
    
    - Split homebrew.sh into a few functions that can be conditionally
      called.
    - Cache verified Homebrew install prefix as a symlink in
      ~/.cache/homebrew-prefix to speed up init.
    - Add a `brew` command place holder alias when we detect the machine
      doesn't have a Homebrew installation. When invoked as `brew bootstrap`
      it will execute the Homebrew bootstrapping script as documented on
      https://brew.sh

--- dot.config/shrc/homebrew.sh
+++ dot.config/shrc/homebrew.sh
@@ -1,6 +1,68 @@
 # shellcheck shell=bash
 [[ ${OSTYPE} =~ ^(darwin|linux) ]] || return 0
 
+__cache_homebrew_prefix=~/.cache/homebrew-prefix
+
+_find_homebrew_install() {
+  local prefixes=("${HOME}/.brew") # non standard, homedir install
+  if [[ ${OSTYPE} =~ ^darwin ]]; then
+    prefixes+=("/usr/local")
+  elif [[ ${OSTYPE} =~ ^linux ]]; then
+    prefixes+=("/home/linuxbrew/.linuxbrew")
+  fi
+
+  local prefix
+  for prefix in "${prefixes[@]}"; do
+    local brew="${prefix}/bin/brew"
+    if [[ -x ${brew} && ${prefix} == $(${brew} --prefix) ]]; then
+      echo "${prefix}"
+      return
+    fi
+  done
+}
+
+_setup_homebrew() {
+  # Set environment variables
+  export HOMEBREW_PREFIX="$1"
+  export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX}";
+  export HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar";
+
+  # Update prefix cache
+  ln -sf "${HOMEBREW_PREFIX}" "${__cache_homebrew_prefix}"
+
+  # Homebrew behavior
+  export HOMEBREW_NO_ANALYTICS=1
+  export HOMEBREW_NO_AUTO_UPDATE=1
+  export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
+  export HOMEBREW_NO_EMOJI=1
+  export HOMEBREW_NO_INSECURE_REDIRECT=1
+  export HOMEBREW_NO_GITHUB_API=1
+  if [[ ${OSTYPE} =~ ^darwin ]]; then
+    export HOMEBREW_CASK_OPTS='--appdir=~/Applications'
+  fi
+
+  # Add to PATHs
+  _move_to_path_head "${HOMEBREW_PREFIX}/bin" "${HOMEBREW_PREFIX}/sbin"
+
+  # macOS will deduce man page paths from the directory of executable but
+  # Linux requires a bit more help.
+  if [[ ${OSTYPE} =~ ^linux ]]; then
+    export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:"; # notice trailing colon
+    export INFOPATH="${HOMEBREW_PREFIX}/share/info${INFOPATH+:$INFOPATH}";
+  fi
+
+  # Load completion files
+  if [[ -n "${ZSH_VERSION-}" ]]; then
+    FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
+  elif [[ -n "${BASH_VERSION-}" ]]; then
+    local cf
+    for cf in "${HOMEBREW_PREFIX}"/etc/bash_completion.d/*; do
+      # shellcheck source=/dev/null # don't follow to check sourced files.
+      source "${cf}"
+    done
+  fi
+}
+
 _move_to_path_head() {
   # Prepend or move arguments to head of $PATH
   local _p
@@ -16,58 +78,26 @@ _move_to_path_head() {
   PATH=$(IFS=:; echo "${_path[*]}")
 }
 
-# Where is Homebrew installed?
-_prefixes=("${HOME}/.brew") # non standard, homedir installation
-if [[ ${OSTYPE} =~ ^linux ]]; then
-  _prefixes+=("/home/linuxbrew/.linuxbrew")
-elif [[ ${OSTYPE} =~ ^darwin ]]; then
-  _prefixes+=("/usr/local")
-fi
-
-for _prefix in "${_prefixes[@]}"; do
-  _brew="${_prefix}/bin/brew"
-  if [[ -x ${_brew} && ${_prefix} == $(${_brew} --prefix) ]]; then
-    # Prepend bin and sbin directories under Homebrew prefix to PATH
-    _move_to_path_head "${_prefix}/bin" "${_prefix}/sbin"
-
-    # Set environment variables and modify PATHs
-    export HOMEBREW_PREFIX="${_prefix}"
-    export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX}";
-    export HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar";
-
-    # macOS will deduce man page paths from the directory of executable but
-    # Linux requires a bit more help.
-    if [[ ${OSTYPE} =~ ^linux ]]; then
-      export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:"; # notice trailing colon
-      export INFOPATH="${HOMEBREW_PREFIX}/share/info${INFOPATH+:$INFOPATH}";
-    fi
-
-    break # We've found our install location and done with the setup
-  fi
-done
-unset _prefixes _prefix _brew
-unset -f _move_to_path_head
-
-if [[ -n ${HOMEBREW_PREFIX+defined} ]]; then
-  # Homebrew behavior options
-  export HOMEBREW_NO_ANALYTICS=1
-  export HOMEBREW_NO_AUTO_UPDATE=1
-  export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
-  export HOMEBREW_NO_EMOJI=1
-  export HOMEBREW_NO_INSECURE_REDIRECT=1
-  export HOMEBREW_NO_GITHUB_API=1
-  if [[ ${OSTYPE} =~ ^darwin ]]; then
-    export HOMEBREW_CASK_OPTS='--appdir=~/Applications'
+_brew_placeholder () {
+  if [[ -n $1 && $1 == "bootstrap" ]]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" 
+    return
   fi
 
-  # Load completion files
-  if [[ -n "${ZSH_VERSION-}" ]]; then
-    FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
-  elif [[ -n "${BASH_VERSION-}" ]]; then
-    for _f in "${HOMEBREW_PREFIX}"/etc/bash_completion.d/*; do
-      # shellcheck source=/dev/null # don't follow to check sourced files.
-      source "${_f}"
-    done
-    unset _f
+  local prefix=$(_find_homebrew_install)
+  if [[ -n ${prefix} ]]; then
+    _setup_homebrew "${prefix}"
+    unalias brew
+  else
+    echo 'Use `brew bootstrap` to install Homebrew.'
   fi
+}
+
+if [[ -L ${__cache_homebrew_prefix} ]]; then
+  _cached_prefix=$(readlink "${__cache_homebrew_prefix}")
+  if [[ -d ${_cached_prefix} ]]; then
+    _setup_homebrew "${_cached_prefix}"
+  fi
+else
+    alias brew="_brew_placeholder"
 fi
