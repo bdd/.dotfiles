d98129a312461fe858a6ca09cb75828b34f8e63f
Berk D. Demir <bdd@mindcast.org>
2021-01-13 18:14:28 -0800

    shrc/editor: boy was i wrong...
    'command' doesn't accept multiple flags (go figure).
    Moreover the behavior of '-p' isn't save between Zsh and Bash...
    
    Use 'whence -p' under Zsh and 'type -p' under Bash.

--- dot.config/shrc/editor.sh
+++ dot.config/shrc/editor.sh
@@ -3,7 +3,11 @@
 # Pick the available EDITOR in order of preference.
 # For commands found, `whence` will print out the resolved paths or value of alias or function name.
 # Remember: "Ed is the standard text editor." --ed(1)
-_editor=$(command -pv vim vi ed | head -n 1)
+whence_or_type="whence"
+[[ -n "${BASH_VERSION-}" ]] && whence_or_type="type"
+
+_editor=$(${whence_or_type} -p vim vi ed | head -n 1)
+
 if [[ -n ${_editor} ]]; then
   export EDITOR=${_editor} VISUAL=${_editor}
 fi
