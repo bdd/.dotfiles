553219898cbdd72720005ce26a67e1ebea26ae4b
Berk D. Demir <bdd@mindcast.org>
2021-11-15 00:07:42 -0800

    shrc/ssh: macos and linux compat for finding user's ssh-agent

--- dot.config/shrc/ssh.sh
+++ dot.config/shrc/ssh.sh
@@ -19,8 +19,41 @@ ssh:restore-gpg-agent() {
 }
 
 ssh:restore-ssh-agent() {
-  ssh:_set-ssh_auth_sock \
-    "$(lsof -a -U -u "${USER}" -c ssh-agent -Fn | sed -n 's/^n//p')"
+  # "Why not just use lsof instead?"
+  # ssh-agent disables ptrace on all platforms[1] leading to different
+  # side effects across operating systems.
+  #
+  # On Linux this causes /proc/<pid>/* files to be only readable by root,
+  # so unprivileged user cannot see the Unix domain socket paths of the
+  # processes owned by themselves.
+  #
+  # [1]: https://git.io/J1Ugs
+
+  local s sockets=() listeners=()
+  IFS=$'\n' sockets+=( \
+    $(find -E "${TMPDIR-/tmp}" -type s -uid "$(id -u)" -regex "${TMPDIR}/ssh-.[^/]+/agent\.[0-9]+" 2>/dev/null) \
+  )
+  for s in "${sockets[@]}"; do
+    # can we connect to this socket or is it a leftover?
+    if nc -U "${s}" < /dev/null; then
+      listeners+=("${s}")
+    fi
+  done
+
+  local n=${#listeners[@]}
+  if ! ((n == 1)); then
+    if [[ ${n} -gt 1 ]]; then
+      echo "more than one eligible listener: ${n}" >&2
+    fi
+    return 1
+  fi
+
+  # "Why loop here?"
+  # Zsh's 1-indexed arrays vs Bash's 0-indexed arrays.
+  # ...and if we made it here, array size is 1.
+  for s in "${listeners[@]}"; do
+    ssh:_set-ssh_auth_sock "${s}"
+  done
 }
 
 case "${OSTYPE}" in
