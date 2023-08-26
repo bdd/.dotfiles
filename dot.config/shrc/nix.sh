c26785ac871436b156717a17f099778f46eed65f
Berk D. Demir <bdd@mindcast.org>
2023-08-26 16:10:29 -0700

    shrc/nix: Rename nixhelp namespace to nixdev
    - Add usage to nixdev:github-ref-hash

--- dot.config/shrc/nix.sh
+++ dot.config/shrc/nix.sh
@@ -1,7 +1,11 @@
 # shellcheck shell=bash
 
-nixhelp:github-ref-hash() {
+nixdev:github-ref-hash() {
   local user=$1 repo=$2 ref=$3
+  if [[ -z ${user-} || -z ${repo-} || -z ${ref-} ]]; then
+    echo "usage: nixhelp:github-ref-hash user repo ref" >&2
+    return 1
+  fi
   local url="https://github.com/${user}/${repo}/archive/${ref}.tar.gz"
   nix hash to-sri sha256:"$(nix-prefetch-url --unpack "${url}")"
 }
