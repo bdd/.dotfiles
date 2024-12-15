b0dfe51546a3ffcf3009f76f30a2374cb05c84b9
Berk D. Demir <bdd@mindcast.org>
2024-12-14 22:24:49 -0800

    shrc/ls.sh: OpenBSD and NetBSD ls doesn't do -G

--- dot.config/shrc/ls.sh
+++ dot.config/shrc/ls.sh
@@ -5,12 +5,14 @@
 LSCOLORS=exfxcxdxbxegedabagacad
 LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
-if [[ $OSTYPE =~ ^(darwin|freebsd|netbsd) ]]; then
-  alias ls='ls -FG'
-  export LSCOLORS
-elif [[ $OSTYPE =~ ^linux ]]; then
+if [[ $OSTYPE =~ ^linux ]]; then
   alias ls='ls -F --color=auto'
   export LS_COLORS
+elif [[ $OSTYPE =~ ^(darwin|freebsd) ]]; then
+  alias ls='ls -FG'
+  export LSCOLORS
+elif [[ $OSTYPE =~ ^(open|net)bsd ]]; then
+  alias ls='ls -F'
 fi
 
 
