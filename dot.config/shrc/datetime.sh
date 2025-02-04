09afb66b539b2a07b149aa78164d620cb288b873
Berk D. Demir <bdd@mindcast.org>
2025-02-04 13:46:28 -0800

    shrc: Add timestamp formats

--- dot.config/shrc/datetime.sh
+++ dot.config/shrc/datetime.sh
@@ -1,9 +1,14 @@
 dt() {
   local opts
   local fmt="%Y-%m-%dT%H:%M" # ISO 8601
-  case $1 in
+  local tsfmt="%Y%m%dT%H%M%S"
+  case "$@" in
     utc|zulu|z)
       opts=(-u +"${fmt}Z") ;;
+    timestamp|ts)
+      opts=(+"${tsfmt}") ;;
+    utc-timestamp|zulu-timestamp|uts|zts)
+      opts=(-u +"${tsfmt}Z") ;;
     @*)
       # convert from epoch
       opts=(-d "$1" +"${fmt}Z") ;;
